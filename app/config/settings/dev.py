1bab8371fd8a5e4d1673c12164a2912178ef1950
hanyonghee <hanyonghee9264@gmail.com>
2018-11-04 16:12:27 +0900

    전체설정 Dockerfile 구성

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -6,7 +6,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'wps9th.c3odv9dyz53u.ap-northeast-2.rds.amazonaws.com',
+        'NAME': 'ec2_deploy',
+        'USER': 'hanyonghee9264',
+        'PASSWORD': 'han46633^^',
+        'PORT': 5432,
     }
 }
